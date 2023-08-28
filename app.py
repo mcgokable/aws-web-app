d3317dc951e92a7dbecb8b0ee96d34407b65d5e9
Siarhei Shviadko <ew7shv@gmail.com>
2023-08-28 22:33:31 +0300

    Add some endpoints

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI, Depends
-import boto3
+import os
+import shutil
+import uuid
+from pathlib import Path
+from typing import Any
+
+import requests
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 
 # boto3.setup_default_session(profile_name="FullAccessEC2User")
 app = FastAPI()
 
-INSTANCE_ID = "i-0abf051a8e0682019"
+AWS_URL = "http://169.254.169.254"
 
 origins = ["*"]
 
@@ -16,13 +23,60 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-def get_ec2_client():
-    return boto3.client("ec2")
+MEDIA_FOLDER_PATH = "./media"
 
 
 @app.get("/")
-async def instance_info(client=Depends(get_ec2_client)):
-    instance_info = client.describe_instances(InstanceIds=[INSTANCE_ID])                           
-    return {"AZ": instance_info["Reservations"][0]["Instances"][0]["Placement"]["AvailabilityZone"], "region": None}
+async def instance_info():
+    response = requests.get(f"{AWS_URL}/latest/meta-data/placement/availability-zone")
+    return {"AZ": response.text, "region": response.text[:-1]}
+
+
+@app.get("/images/{name}")
+def downloaded_image(name: str):
+    return FileResponse(f"{MEDIA_FOLDER_PATH}/{name}")
+
+
+@app.get("/images/metadata")
+def random_metadata(name: str):
+    ...
+
+
+@app.get("/images/metadata/{name}")
+def get_image_metadata(name: str):
+    ...
+
+
+@app.post("/images")
+def upload_image(file: UploadFile = File(...)):
+    try:
+        filename = file.filename or uuid.uuid4().hex
+        with open(f"./media/{filename}", "wb") as f:
+            shutil.copyfileobj(file.file, f)
+        metadata = create_metadata(filename)
+        
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+    
+    return {"message": f"Successfully uploaded {file.filename}", "metadata": metadata}
+
+
+@app.delete("/images/{name}")
+def delete_image(name: str):
+    if os.path.isfile(f"{MEDIA_FOLDER_PATH}/{name}"):
+        os.remove(f"{MEDIA_FOLDER_PATH}/{name}")
+        return {"message": "File successfully deleted"}
+    else:
+        return {"message": "NO such file"}
+
+
+def create_metadata(filename: str) -> dict[str, Any]:
+    stat = os.stat(f"{MEDIA_FOLDER_PATH}/{filename}")
+    return {
+        "size": stat.st_size,
+        "updated_at": stat.st_mtime,
+        "extension": Path(filename).suffix,
+        "name": Path(filename).stem,
+    }
